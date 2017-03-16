e3ce2df4db30d7823dc7a7b648a22b59d2c8e558
adabotcon <adabotcon@outlook.com>
2017-03-16 14:55:58 -0700

    Fixing test

--- test/test-quotes.js
+++ test/test-quotes.js
@@ -1,10 +1,39 @@
-const mocha = require('mocha');
 const chai = require('chai');
-const chaiHTTP = require('chai-http');
+const chaiHttp = require('chai-http');
 
-const isQuote = require('../isQuote');
+const {app, runServer, closeServer} = require('../server');
 
-describe('isQuote', function() {
-    it('should give right answers for equal and unequal inputs');
-  }
-);
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Users', function() {
+  // Before our tests run, we activate the server. Our `runServer`
+  // function returns a promise, and we return the promise by
+  // doing `return runServer`. If we didn't return a promise here,
+  // there's a possibility of a race condition where our tests start
+  // running before our server has started.
+  before(function() {
+    return runServer();
+  });
+
+  // Close server after these tests run in case
+  // we have other test modules that need to 
+  // call `runServer`. If server is already running,
+  // `runServer` will error out.
+  after(function() {
+    return closeServer();
+  });
+  // `chai.request.get` is an asynchronous operation. When
+  // using Mocha with async operations, we need to either
+  // return an ES6 promise or else pass a `done` callback to the
+  // test that we call at the end. We prefer the first approach, so
+  // we just return the chained `chai.request.get` object.
+  it('should list users on GET', function() {
+    return chai.request(server)
+      .get('/')
+      .then(function(res) {
+        res.should.have.status(200);
+        });
+      });
+  });
