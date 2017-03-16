45815b1aa97e71cd50f21f7f2b6963a259a530ce
adabotcon <adabotcon@outlook.com>
2017-03-16 14:36:50 -0700

    Adding Travis CI

---
+++ test/test-quotes.js
@@ -0,0 +1,10 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+
+const isQuote = require('../isQuote');
+
+describe('isQuote', function() {
+    it('should give right answers for equal and unequal inputs');
+  }
+);
