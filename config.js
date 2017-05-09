668ae1f3bb76659c357a49beb71ad184332687c8
adabotcon <adabotcon@outlook.com>
2017-05-09 16:14:15 -0700

    Adding mLab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/quotesData';
+                      'mongodb://adabotcon:3169Quotes@ds131151.mlab.com:31151/quotesholic';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/quotesData-test');
